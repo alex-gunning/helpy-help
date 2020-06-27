f3dbcc1c44c8098dab6be35b86e856c8e5ce5fbd
Alex Gunning <atm.gunning@gmail.com>
2020-06-28 01:34:13 +0200

    app scaffolding

---
+++ src/main/kotlin/org/helpy/helpcustomerservice/HelpCustomerServiceApplication.kt
@@ -0,0 +1,11 @@
+package org.helpy.helpcustomerservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HelpCustomerServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<HelpCustomerServiceApplication>(*args)
+}
---
+++ src/test/kotlin/org/helpy/helpcustomerservice/HelpCustomerServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.helpy.helpcustomerservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HelpCustomerServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
