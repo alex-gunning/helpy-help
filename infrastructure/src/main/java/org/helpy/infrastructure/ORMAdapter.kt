a01dbba370139aa2484cf36bc0af2b6eb4270cc4
Alex Gunning <atm.gunning@gmail.com>
2020-07-04 22:02:02 +0200

    Get local builds working

--- infrastructure/src/main/java/org/helpy/infrastructure/ORMAdapter.kt
+++ infrastructure/src/main/java/org/helpy/infrastructure/ORMAdapter.kt
@@ -3,9 +3,8 @@ package org.helpy.infrastructure
 import org.helpy.domain.Customer
 import org.helpy.domain.CustomerService
 import org.springframework.stereotype.Repository
-import org.springframework.stereotype.Service
 
-@Service
+@Repository
 class ORMAdapter: CustomerService {
     override fun save(customer: Customer): Customer {
         return Customer("Alex", "Gunning", "091209")
--- web/src/main/java/org/helpy/web/CustomController.kt
+++ web/src/main/java/org/helpy/web/CustomController.kt
@@ -1,9 +1,6 @@
-package org.helpy.web.controllers
+package org.helpy.web
 
-import org.helpy.domain.Customer
 import org.helpy.domain.CustomerService
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
